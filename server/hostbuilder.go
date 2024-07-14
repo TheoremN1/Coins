193718c48492b0e628768d1535baaf58420ed97a
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-14 23:39:03 +0500

    Подключена ORM

--- server/hostbuilder.go
+++ server/hostbuilder.go
@@ -4,18 +4,24 @@ import (
 	"encoding/json"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/TheoremN1/Coins/configs"
 	"github.com/TheoremN1/Coins/server/controllers"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Host struct {
-	Router *Router
+	Database         *gorm.DB
+	IndexController  *controllers.IndexController
+	HealthController *controllers.HealthController
+	Router           *Router
 }
 
 func NewHost() *Host {
-	confFile, err := os.Open("configs/config.json")
+	confFile, err := os.Open(filepath.Join("configs", "config.json"))
 	if err != nil {
 		panic(err)
 	}
@@ -29,15 +35,29 @@ func NewHost() *Host {
 	if err != nil {
 		panic(err)
 	}
-	indexController := controllers.NewIndexController()
-	healthController := controllers.NewHealthController()
 
-	router := NewRouter(
+	stringConnection := "host=" + conf.Database.Host +
+		" user=" + conf.Database.Username +
+		" dbname=" + conf.Database.Name +
+		" password=" + conf.Database.Password
+
+	database, err := gorm.Open(postgres.Open(stringConnection), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	host := Host{}
+	host.Database = database
+
+	host.IndexController = controllers.NewIndexController()
+	host.HealthController = controllers.NewHealthController()
+
+	host.Router = NewRouter(
 		gin.Default(),
-		indexController,
-		healthController,
+		host.IndexController,
+		host.HealthController,
 		conf.Server.Host+":"+conf.Server.Port,
 	)
 
-	return &Host{router}
+	return &host
 }
