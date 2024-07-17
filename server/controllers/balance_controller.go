9407e0b54eb79fff44cd427ea3c943ebb57f2759
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-17 13:16:52 +0500

    Проведен рефакторинг контроллеров баланса и пользователей

--- server/controllers/balance_controller.go
+++ server/controllers/balance_controller.go
@@ -26,62 +26,60 @@ func (balanceController *BalanceController) IsUserExist(id string) bool {
 }
 
 func (balanceController *BalanceController) Get(context *gin.Context) {
-	id := context.Request.URL.Query().Get("id")
-	var status int
-	var user models.User
-	if id == "" {
-		status = http.StatusBadRequest
-	} else {
-		if balanceController.IsUserExist(id) {
-			status = http.StatusOK
-			balanceController.database.
-				Where("id = ?", id).
-				First(&user)
-		} else {
-			status = http.StatusBadRequest
-		}
-	}
-
-	context.JSON(status, gin.H{
-		"balance": user.Balance,
-		"status":  status,
-	})
-}
-
-func (balanceController *BalanceController) Put(context *gin.Context) {
+	// Input
 	query := context.Request.URL.Query()
-	var status int
 	id := query.Get("id")
+	// Output
+	var status int
+	var hash gin.H
+
 	if id != "" && balanceController.IsUserExist(id) {
 		var user models.User
 		balanceController.database.
 			Where("id = ?", id).
 			First(&user)
-		switch query.Get("action") {
+		status = http.StatusOK
+		hash = gin.H{"balance": user.Balance}
+	} else {
+		status = http.StatusBadRequest
+		hash = nil
+	}
+
+	context.JSON(status, hash)
+}
+
+func (balanceController *BalanceController) Put(context *gin.Context) {
+	// Input
+	query := context.Request.URL.Query()
+	id := query.Get("id")
+	action := query.Get("action")
+	amount, err := strconv.Atoi(query.Get("amount"))
+	// Output
+	var status int
+
+	if (action == "plus" || action == "minus") &&
+		id != "" &&
+		err == nil &&
+		amount > 0 &&
+		balanceController.IsUserExist(id) {
+
+		var user models.User
+		balanceController.database.
+			Where("id = ?", id).
+			First(&user)
+
+		switch action {
 		case "plus":
-			amount, err := strconv.Atoi(query.Get("amount"))
-			if err == nil {
-				user.Balance += amount
-				balanceController.database.Save(&user)
-				status = http.StatusOK
-			} else {
-				status = http.StatusBadRequest
-			}
+			user.Balance += amount
 		case "minus":
-			amount, err := strconv.Atoi(query.Get("amount"))
-			if err == nil {
-				user.Balance -= amount
-				balanceController.database.Save(&user)
-				status = http.StatusOK
-			} else {
-				status = http.StatusBadRequest
-			}
-		default:
-			status = http.StatusBadRequest
+			user.Balance -= amount
 		}
+
+		balanceController.database.Save(&user)
+		status = http.StatusOK
 	} else {
 		status = http.StatusBadRequest
 	}
 
-	context.JSON(status, gin.H{})
+	context.JSON(status, nil)
 }
--- server/controllers/user_controller.go
+++ server/controllers/user_controller.go
@@ -42,10 +42,15 @@ func (userController *UserController) IsUserExist(id string) bool {
 }
 
 func (userController *UserController) Get(context *gin.Context) {
-	id := context.Request.URL.Query().Get("id")
+	// Input
+	query := context.Request.URL.Query()
+	id := query.Get("id")
+	// Output
 	var status int
 	var hash gin.H
+
 	if id == "" {
+		// All users
 		var users []*models.User
 		hash = gin.H{}
 		userController.database.Model(&models.User{}).Find(&users)
@@ -61,8 +66,10 @@ func (userController *UserController) Get(context *gin.Context) {
 			}
 		} else {
 			status = http.StatusBadRequest
+			hash = nil
 		}
 	} else {
+		// Only one user
 		var user models.User
 		if userController.IsUserExist(id) {
 			status = http.StatusOK
@@ -77,71 +84,92 @@ func (userController *UserController) Get(context *gin.Context) {
 			}
 		} else {
 			status = http.StatusBadRequest
-			hash = gin.H{}
+			hash = nil
 		}
-
 	}
+
 	context.JSON(status, hash)
 }
 
 func (userController *UserController) Post(context *gin.Context) {
+	// Input
 	query := context.Request.URL.Query()
+	username := query.Get("username")
+	name := query.Get("name")
+	surname := query.Get("surname")
+	role := query.Get("role")
+	// Output
 	var status int
 	var hash gin.H
-	if userController.IsUsernameFree(query.Get("username")) &&
-		userController.IsRoleExist(query.Get("role")) {
-		var user models.User
-		var role models.Role
-		user = models.User{
-			Username: query.Get("username"),
-			Name:     query.Get("name"),
-			Surname:  query.Get("surname"),
+
+	if userController.IsUsernameFree(username) &&
+		userController.IsRoleExist(role) {
+		// Username is free and role is exist
+		user := models.User{
+			Username: username,
+			Name:     name,
+			Surname:  surname,
 			Balance:  0,
 		}
 		userController.database.Save(&user)
+
+		var role models.Role
 		userController.database.
-			Where("key = ?", query.Get("role")).
+			Where("key = ?", role).
 			First(&role)
-		role.Users = append(role.Users, user)
+		role.Users = append(role.Users, user) // TODO: Не уверен что в БД записывается связь между юзером и ролью
+
 		status = http.StatusOK
 		hash = gin.H{"id": user.Id}
 	} else {
 		status = http.StatusBadRequest
-		hash = gin.H{}
+		hash = nil
 	}
+
 	context.JSON(status, hash)
 }
 
 func (userController *UserController) Put(context *gin.Context) {
+	// Input
 	query := context.Request.URL.Query()
+	id := query.Get("id")
+	name := query.Get("name")
+	surname := query.Get("surname")
+	// Output
 	var status int
-	if userController.IsUserExist(query.Get("id")) {
+
+	if userController.IsUserExist(id) {
 		var user models.User
 		userController.database.
-			Where("id = ?", query.Get("id")).
+			Where("id = ?", id).
 			First(&user)
-		user.Name = query.Get("name")
-		user.Surname = query.Get("surname")
+		user.Name = name
+		user.Surname = surname
 		userController.database.Save(user)
 		status = http.StatusOK
 	} else {
 		status = http.StatusBadRequest
 	}
-	context.JSON(status, gin.H{})
+	context.JSON(status, nil)
 }
 
 func (userController *UserController) Delete(context *gin.Context) {
+	// Input
 	query := context.Request.URL.Query()
+	id := query.Get("id")
+	// Output
 	var status int
-	if userController.IsUserExist(query.Get("id")) {
+
+	if userController.IsUserExist(id) {
 		var user models.User
 		userController.database.
-			Where("id = ?", query.Get("id")).
+			Where("id = ?", id).
 			First(&user)
 		userController.database.Delete(&user)
 		status = http.StatusOK
 	} else {
 		status = http.StatusBadRequest
 	}
-	context.JSON(status, gin.H{})
+
+	context.JSON(status, nil)
 }
