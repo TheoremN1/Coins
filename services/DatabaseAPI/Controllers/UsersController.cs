e180c1e83c7ffa6e597a7a3db0218c835d5ffedb
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-22 18:34:52 +0500

    Сделал методы асинхронными.

--- services/DatabaseAPI/Controllers/UsersController.cs
+++ services/DatabaseAPI/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using DatabaseAPI.Database;
+﻿using DatabaseAPI.Database;
 using DatabaseAPI.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseAPI.Controllers;
 
@@ -8,66 +9,66 @@ namespace DatabaseAPI.Controllers;
 [ApiController]
 public class UsersController(DatabaseContext context) : ControllerBase
 {
-    private readonly DatabaseContext _context = context;
+	private readonly DatabaseContext _context = context;
 
-    // GET: api/<UsersController>
-    [HttpGet]
-    public IEnumerable<User> Get()
-    {
-        return _context.Users;
-    }
+	// GET: api/<UsersController>
+	[HttpGet]
+	public IEnumerable<User> Get()
+	{
+		return _context.Users;
+	}
 
-    // GET api/<UsersController>/5
-    [HttpGet("{id}")]
-    public User? Get(int id)
-    {
-        return _context.Users.FirstOrDefault(u => u.Id == id);
-    }
+	// GET api/<UsersController>/5
+	[HttpGet("{id}")]
+	public async Task<User?> Get(int id)
+	{
+		return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+	}
 
-    // POST api/<UsersController>
-    [HttpPost]
-    public bool Post([FromForm] User user)
-    {
-        if (_context.Users.Any(u => u.Login == user.Login || u.Id == user.Id))
-            return false;
+	// POST api/<UsersController>
+	[HttpPost]
+	public async Task<bool> Post([FromForm] User user)
+	{
+		if (await _context.Users.AnyAsync(u => u.Login == user.Login || u.Id == user.Id))
+			return false;
 
-        _context.Users.Add(user);
-        _context.SaveChanges();
-        return true;
-    }
+		await _context.Users.AddAsync(user);
+		await _context.SaveChangesAsync();
+		return true;
+	}
 
-    // PUT api/<UsersController>/5
-    [HttpPut("{id}")]
-    public bool Put(int id, [FromForm] User newUser)
-    {
-        var oldUser = _context.Users.FirstOrDefault(u => u.Id == id);
-        if (oldUser is null)
-            return false;
+	// PUT api/<UsersController>/5
+	[HttpPut("{id}")]
+	public async Task<bool> Put(int id, [FromForm] User newUser)
+	{
+		var oldUser = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+		if (oldUser is null)
+			return false;
 
-        if (_context.Users.Any(u => u.Login == newUser.Login && u.Id != id))
-            return false;
+		if (await _context.Users.AnyAsync(u => u.Login == newUser.Login && u.Id != id))
+			return false;
 
-        oldUser.Name = newUser.Name;
-        oldUser.Surname = newUser.Surname;
-        oldUser.Balance = newUser.Balance;
-        oldUser.Login = newUser.Login;
-        oldUser.Password = newUser.Password;
-        oldUser.RoleKey = newUser.RoleKey;
+		oldUser.Name = newUser.Name;
+		oldUser.Surname = newUser.Surname;
+		oldUser.Balance = newUser.Balance;
+		oldUser.Login = newUser.Login;
+		oldUser.Password = newUser.Password;
+		oldUser.RoleKey = newUser.RoleKey;
 
-        _context.SaveChanges();
-        return true;
-    }
+		await _context.SaveChangesAsync();
+		return true;
+	}
 
-    // DELETE api/<UsersController>/5
-    [HttpDelete("{id}")]
-    public bool Delete(int id)
-    {
-        var user = _context.Users.FirstOrDefault(u => u.Id == id);
-        if (user is null)
-            return false;
+	// DELETE api/<UsersController>/5
+	[HttpDelete("{id}")]
+	public async Task<bool> Delete(int id)
+	{
+		var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+		if (user is null)
+			return false;
 
-        _context.Users.Remove(user);
-        _context.SaveChanges();
-        return true;
-    }
+		_context.Users.Remove(user);
+		await _context.SaveChangesAsync();
+		return true;
+	}
 }
