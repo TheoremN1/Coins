6d11805d982821499696f2c6c1bf255f8c675d0a
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-22 19:13:53 +0500

    Реализован контроллер мерча

--- services/DatabaseAPI/Controllers/MerchController.cs
+++ services/DatabaseAPI/Controllers/MerchController.cs
@@ -1,40 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatabaseAPI.Database;
+using DatabaseAPI.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class MerchController : ControllerBase
+public class MerchController(DatabaseContext context) : ControllerBase
 {
+    private readonly DatabaseContext _context = context;
+
     // GET: api/<MerchController>
     [HttpGet]
-    public IEnumerable<string> Get()
+    public IEnumerable<Merch> Get()
     {
-        return new string[] { "value1", "value2" };
+        return _context.Merches;
     }
 
     // GET api/<MerchController>/5
     [HttpGet("{id}")]
-    public string Get(int id)
+    public async Task<Merch?> Get(int id)
     {
-        return "value";
+        return await _context.Merches.FirstOrDefaultAsync(m => m.Id == id);
     }
 
     // POST api/<MerchController>
     [HttpPost]
-    public void Post([FromBody] string value)
+    public async Task<bool> Post([FromForm] Merch merch)
     {
+        if (await _context.Merches.AnyAsync(m => m.Id == merch.Id))
+            return false;
+
+        await _context.Merches.AddAsync(merch);
+        await _context.SaveChangesAsync();
+        return true;
     }
 
     // PUT api/<MerchController>/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public async Task<bool> Put(int id, [FromBody] Merch newMerch)
     {
+        var oldMerch = await _context.Merches.FirstOrDefaultAsync(m => m.Id == id);
+        if (oldMerch is null)
+            return false;
+
+        oldMerch.Name = newMerch.Name;
+        oldMerch.Description = newMerch.Description;
+        oldMerch.Price = newMerch.Price;
+
+        await _context.SaveChangesAsync();
+        return true;
     }
 
     // DELETE api/<MerchController>/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public async Task<bool> Delete(int id)
     {
+        var merch = await _context.Merches.FirstOrDefaultAsync(m => m.Id == id);
+        if (merch is null)
+            return false;
+
+        _context.Merches.Remove(merch);
+        await _context.SaveChangesAsync();
+        return true;
     }
 }
