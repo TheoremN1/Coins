775616a2e4223bd9aba12b1b4685ee3d13b0b751
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-22 18:18:16 +0500

    Реализовал функционал контроллера юзеров.

--- services/DatabaseAPI/Controllers/UsersController.cs
+++ services/DatabaseAPI/Controllers/UsersController.cs
@@ -1,40 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DatabaseAPI.Database;
+using DatabaseAPI.Database.Models;
 
 namespace DatabaseAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UsersController : ControllerBase
+public class UsersController(DatabaseContext context) : ControllerBase
 {
+    private readonly DatabaseContext _context = context;
+
     // GET: api/<UsersController>
     [HttpGet]
-    public IEnumerable<string> Get()
+    public IEnumerable<User> Get()
     {
-        return new string[] { "value1", "value2" };
+        return _context.Users;
     }
 
     // GET api/<UsersController>/5
     [HttpGet("{id}")]
-    public string Get(int id)
+    public User? Get(int id)
     {
-        return "value";
+        return _context.Users.FirstOrDefault(u => u.Id == id);
     }
 
     // POST api/<UsersController>
     [HttpPost]
-    public void Post([FromBody] string value)
+    public bool Post([FromForm] User user)
     {
+        if (_context.Users.Any(u => u.Login == user.Login || u.Id == user.Id))
+            return false;
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+        return true;
     }
 
     // PUT api/<UsersController>/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public bool Put(int id, [FromForm] User newUser)
     {
+        var oldUser = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (oldUser is null)
+            return false;
+
+        if (_context.Users.Any(u => u.Login == newUser.Login && u.Id != id))
+            return false;
+
+        oldUser.Name = newUser.Name;
+        oldUser.Surname = newUser.Surname;
+        oldUser.Balance = newUser.Balance;
+        oldUser.Login = newUser.Login;
+        oldUser.Password = newUser.Password;
+        oldUser.RoleKey = newUser.RoleKey;
+
+        _context.SaveChanges();
+        return true;
     }
 
     // DELETE api/<UsersController>/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public bool Delete(int id)
     {
+        var user = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (user is null)
+            return false;
+
+        _context.Users.Remove(user);
+        _context.SaveChanges();
+        return true;
     }
 }
