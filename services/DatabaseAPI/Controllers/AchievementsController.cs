71bb5924d786a4599939565ac57bdaf3db0902a4
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-22 18:47:57 +0500

    Реализовал контроллер достижений

--- services/DatabaseAPI/Controllers/AchievementsController.cs
+++ services/DatabaseAPI/Controllers/AchievementsController.cs
@@ -1,40 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatabaseAPI.Database;
+using DatabaseAPI.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class AchievementsController : ControllerBase
+public class AchievementsController(DatabaseContext context) : ControllerBase
 {
-    // GET: api/<AchievementsController>
-    [HttpGet]
-    public IEnumerable<string> Get()
-    {
-        return new string[] { "value1", "value2" };
+	private readonly DatabaseContext _context = context;
+
+	// GET: api/<AchievementsController>
+	[HttpGet]
+	public IEnumerable<Achievement> Get()
+	{
+		return _context.Achievements;
+	}
+
+	// GET api/<AchievementsController>/5
+	[HttpGet("{id}")]
+	public async Task<Achievement?> Get(int id)
+	{
+		return await _context.Achievements.FirstOrDefaultAsync(a => a.Id == id);
+	}
+
+	// POST api/<AchievementsController>
+	[HttpPost]
+	public async Task<bool> Post([FromForm] Achievement achievement)
+	{
+		if (await _context.Achievements.AnyAsync(a => a.Id == achievement.Id))
+			return false;
+
+		await _context.Achievements.AddAsync(achievement);
+		await _context.SaveChangesAsync();
+		return true;
+	}
+
+	// PUT api/<AchievementsController>/5
+	[HttpPut("{id}")]
+	public async Task<bool> Put(int id, [FromForm] Achievement newAchievement)
+	{
+		var oldAchievement = await _context.Achievements.FirstOrDefaultAsync(a => a.Id == id);
+		if (oldAchievement is null)
+			return false;
+
+		oldAchievement.Name = newAchievement.Name;
+		oldAchievement.Description = newAchievement.Description;
+		oldAchievement.Reward = newAchievement.Reward;
+
+		await _context.SaveChangesAsync();
+		return true;
     }
 
-    // GET api/<AchievementsController>/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
+	// DELETE api/<AchievementsController>/5
+	[HttpDelete("{id}")]
+	public async Task<bool> Delete(int id)
+	{
+		var achievement = await _context.Achievements.FirstOrDefaultAsync(a => a.Id == id);
+		if (achievement is null)
+			return false;
 
-    // POST api/<AchievementsController>
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
-
-    // PUT api/<AchievementsController>/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/<AchievementsController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
+		_context.Achievements.Remove(achievement);
+		await _context.SaveChangesAsync();
+		return true;
     }
 }
