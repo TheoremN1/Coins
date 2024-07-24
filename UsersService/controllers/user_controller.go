65a5f291ae9a3ae006713281e2021809631a8d52
M0untain13 <dan2002.dan2002@mail.ru>
2024-07-24 23:19:42 +0500

    Переписал методы в контроллере юзеров

--- UsersService/controllers/user_controller.go
+++ UsersService/controllers/user_controller.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/TheoremN1/Coins/UsersService/models"
+	"github.com/dghubble/sling"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,20 +19,65 @@ func NewUserController(databaseUrl string) *UserController {
 }
 
 func (userController *UserController) Get(context *gin.Context) {
-	responce, _ := http.Get(userController.databaseUrl + "/api/users")
-	var j interface{}
-	json.NewDecoder(responce.Body).Decode(&j)
-	context.JSON(http.StatusOK, j)
+	query := context.Request.URL.Query()
+	id := query.Get("id")
+
+	url := userController.databaseUrl + "/api/users/" + id
+	client := &http.Client{}
+	req, _ := sling.New().Get(url).Request()
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	var user *models.User
+	json.NewDecoder(resp.Body).Decode(&user)
+
+	context.JSON(resp.StatusCode, user)
 }
 
 func (userController *UserController) Post(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"type": "post"})
+	var user *models.User
+	json.NewDecoder(context.Request.Body).Decode(&user)
+
+	url := userController.databaseUrl + "/api/users"
+	client := &http.Client{}
+	req, _ := sling.New().Post(url).BodyForm(user).Request()
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	var isPosted bool
+	json.NewDecoder(resp.Body).Decode(&isPosted)
+
+	context.JSON(resp.StatusCode, isPosted)
 }
 
 func (userController *UserController) Put(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"type": "put"})
+	var user *models.User
+	json.NewDecoder(context.Request.Body).Decode(&user)
+
+	url := userController.databaseUrl + "/api/users/" + strconv.Itoa(user.Id)
+	client := &http.Client{}
+	req, _ := sling.New().Put(url).BodyForm(user).Request()
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	var isPuted bool
+	json.NewDecoder(resp.Body).Decode(&isPuted)
+
+	context.JSON(resp.StatusCode, isPuted)
 }
 
 func (userController *UserController) Delete(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"type": "delete"})
+	query := context.Request.URL.Query()
+	id := query.Get("id")
+
+	url := userController.databaseUrl + "/api/users/" + id
+	client := &http.Client{}
+	req, _ := sling.New().Delete(url).Request()
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	var isDeleted *bool
+	json.NewDecoder(resp.Body).Decode(&isDeleted)
+
+	context.JSON(resp.StatusCode, isDeleted)
 }
